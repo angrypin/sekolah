6fd66736ab3b0e5f7057210fa8ae439d8378caab
angrypin <arifinmblygf@gmail.com>
2021-09-10 11:25:49 +0700

    rename connect.php to config.php and move to assets/php

---
+++ assets/php/config.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+const TITLE = 'SMK Negeri 9 LoFI';
+
+$_SESSION['loged_in'] = isset($_SESSION['loged_in']) ? true : false;
+$config = array(
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'myxampp',
+  'database' => 'lofi'
+);
+
+$sql = new PDO(
+  "mysql:host={$config['host']}; dbname={$config['database']}",
+  $config['username'],
+  $config['password']
+);
+
+$rows = [];
+$name_regex = '^[A-Za-z0-9_\-]+$';
+
+function valid_username($name) {
+  global $name_regex;
+  return (
+    strlen($name) > 0 &&
+    strlen($name) <= 15 &&
+    preg_match("/$name_regex/", $name) === 1
+  );
+}
+?>
